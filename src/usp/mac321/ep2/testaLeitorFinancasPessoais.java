9f68b262eb98bfdbe62be9efd60c7301771e7ca0
João Ricardo <joaoricardorodriguesribeiro@usp.br>
2024-06-25 21:12:26 -0300

    correção de arquivos

--- src/usp/mac321/ep2/testaLeitorFinancasPessoais.java
+++ src/usp/mac321/ep2/testaLeitorFinancasPessoais.java
@@ -2,9 +2,19 @@ package usp.mac321.ep2;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-class testaLeitorFinancasPessoais {
+class TestaLeitorFinancasPessoais {
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
 
 	@Test
 	void test() {
